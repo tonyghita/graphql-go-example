6b1bda5b9dfe51b5d319f20ac3a1dff9439b3e08
Tony Ghita <tony@twitch.tv>
2017-12-02 06:33:54 -0800

    Improve documentation

--- schema/schema.go
+++ schema/schema.go
@@ -1,17 +1,15 @@
-// Package schema ...
-//
 // TODO:
 // 	- explain why I use .graphql files to define the schema
 // 	- explain why we embed the .graphql files in the binary.
 // 	- explain why this file is necessary and how the method is used.
-package schema
-
-import "bytes"
-
+//
 // Use `go generate` to pack all *.graphql files under this directory (and sub-directories) into
 // a binary format.
 //
 //go:generate go-bindata -ignore=\.go -pkg=schema -o=bindata.go ./...
+package schema
+
+import "bytes"
 
 // String reads the .graphql schema files from the generated _bindata.go file, concatenating the
 // files together into one string.
@@ -24,6 +22,7 @@ func String() string {
 		b := MustAsset(name)
 		buf.Write(b)
 
+		// Add a newline if the file does not end in a newline.
 		if len(b) > 0 && b[len(b)-1] != '\n' {
 			buf.WriteByte('\n')
 		}
