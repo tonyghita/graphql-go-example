c940df5dad0fb3e2c91db2a98641063e0268168a
Tony Ghita <tony@twitch.tv>
2018-01-15 13:50:29 -0800

    Use sub-tests for test cases

--- handler/graphiql_test.go
+++ handler/graphiql_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -30,22 +31,24 @@ func TestGraphiQLHandler(t *testing.T) {
 	for _, test := range tests {
 		test := test
 
-		req, err := http.NewRequest(test.method, ts.URL, nil)
-		fatalIfErr(t, err)
+		t.Run(fmt.Sprintf("%s request returns HTTP %d", test.method, test.status), func(t *testing.T) {
+			req, err := http.NewRequest(test.method, ts.URL, nil)
+			fatalIfErr(t, err)
 
-		resp, err := ts.Client().Do(req)
-		fatalIfErr(t, err)
+			resp, err := ts.Client().Do(req)
+			fatalIfErr(t, err)
 
-		if resp.StatusCode != test.status {
-			t.Errorf("wrong response code: expected %q, got %q", test.status, resp.StatusCode)
-		}
+			if resp.StatusCode != test.status {
+				t.Errorf("wrong response code: expected %q, got %q", test.status, resp.StatusCode)
+			}
 
-		b, err := ioutil.ReadAll(resp.Body)
-		fatalIfErr(t, err)
+			b, err := ioutil.ReadAll(resp.Body)
+			fatalIfErr(t, err)
 
-		if len(b) == 0 {
-			t.Error("expected response to have a body")
-		}
+			if len(b) == 0 {
+				t.Error("expected response to have a body")
+			}
+		})
 	}
 }
 
