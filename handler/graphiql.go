01681ea95e8fdfe23e8f1fefb5cddb4fcc84fcec
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-02-03 19:29:10 +0100

    Updated graphiql version

--- handler/graphiql.go
+++ handler/graphiql.go
@@ -21,35 +21,36 @@ var graphiql = []byte(`
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.11.11/graphiql.css"/>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/graphiql/0.11.11/graphiql.min.js"></script>
+		<style>body {height: 100vh; margin: 0; width: 100%; overflow: hidden;}</style>
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/graphiql/0.11.11/graphiql.css" />
+		<script src="//cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/graphiql/0.11.11/graphiql.min.js"></script>
 	</head>
-	<body style="width: 100%; height: 100%; margin: 0; overflow: hidden;">
+	<body>
 		<div id="graphiql" style="height: 100vh;">Loading...</div>
-		<script>
-			function fetchGQL(params) {
-				return fetch("/graphql", {
-					method: "post",
-					body: JSON.stringify(params),
-					credentials: "include",
-				}).then(function (resp) {
-					return resp.text();
-				}).then(function (body) {
-					try {
-						return JSON.parse(body);
-					} catch (error) {
-						return body;
-					}
-				});
-			}
-
-			ReactDOM.render(
-				React.createElement(GraphiQL, {fetcher: fetchGQL}),
-				document.getElementById("graphiql")
-			)
+		<script>		
+			function fetchGQL(params) {		
+				return fetch("/graphql", {		
+					method: "post",		
+					body: JSON.stringify(params),		
+					credentials: "include",		
+				}).then(function (resp) {		
+					return resp.text();		
+				}).then(function (body) {		
+					try {		
+						return JSON.parse(body);		
+					} catch (error) {		
+						return body;		
+					}		
+				});		
+			}		
+		
+			ReactDOM.render(		
+				React.createElement(GraphiQL, {fetcher: fetchGQL}),		
+				document.getElementById("graphiql")		
+			)		
 		</script>
 	</body>
 </html>
